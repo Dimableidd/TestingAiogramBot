b634733d1fb1ac5162aca405d65e58d63d73e259
Dimableidd <dmitriy_kostyuk_2015@mail.ru>
2022-11-07 00:07:09 +0300

    Delete sqlite data

--- TestingTeleBOT/TestingTeleBOT.py
+++ TestingTeleBOT/TestingTeleBOT.py
@@ -21,6 +21,13 @@ def sql_start():
     base.execute('CREATE TABLE IF NOT EXISTS menu(img TEXT, name TEXT PRIMARY KEY, description TEXT)')
     base.commit()
 
+async def sql_read2():
+    return cur.execute('SELECT * FROM menu').fetchall()
+
+async def sql_delete_command(data):
+    cur.execute('DELETE FROM menu WHERE name == ?',(data,))
+    base.commit()
+
 async def sql_add_command(state):
     async with state.proxy() as data:
         cur.execute('INSERT INTO menu VALUES (?, ?, ?)', tuple(data.values()))
@@ -29,8 +36,9 @@ b1=KeyboardButton('/help')
 b2=KeyboardButton('/wtf')
 b3=KeyboardButton('/Links')
 b4=KeyboardButton('/You')
+b5=KeyboardButton('/Delete')
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-kb_client.row(b1,b2,b3,b4)
+kb_client.row(b1,b2,b3,b4).add(b5)
 @dp.message_handler(commands = ['start'])
 async def send(message: types.Message):
     await message.answer('<b>Start</b>',reply_markup=kb_client)
@@ -71,6 +79,17 @@ async def menus(message: types.Message):
     for ret in cur.execute('SELECT * FROM menu').fetchall():
         await bot.send_photo(message.from_user.id, ret[0],f'<b>{ret[1]}</b>\n<b>Description</b>: {ret[2]}')
 
+@dp.callback_query_handler(lambda x: x.data and x.data.startswith('del '))
+async def del_callback_run(callback_query: types.CallbackQuery):
+    await sql_delete_command(callback_query.data.replace('del ', ''))
+    await callback_query.answer(text=f'{callback_query.data.replace("del ", "")} delate.',show_alert=True)
+
+@dp.message_handler(commands='Delete')
+async def delete_item(message: types.Message):
+    read = await sql_read2()
+    for ret in read:
+        await bot.send_photo(message.from_user.id, ret[0],f'<b>{ret[1]}</b>\n<b>Description</b>: {ret[2]}')
+        await bot.send_message(message.from_user.id,text='^^^', reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton(f'Delete {ret[1]}', callback_data=f'del {ret[1]}')))
 
 Inkey = InlineKeyboardMarkup(row_width=1)
 urlButton1 = InlineKeyboardButton(text='Link Vk',url='https://vk.com/dimableidd')
